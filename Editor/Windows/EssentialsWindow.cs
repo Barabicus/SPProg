1c08e898b4197126fa65855b685cc24ba42130f1
Barabicus <fiachra14@gmail.com>
2015-02-22 02:16:43 +0000

    update

---
+++ Editor/Windows/EssentialsWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EssentialsWindow : EditorWindow
+{
+
+    [MenuItem("Window/Essential GameObjects")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(EssentialsWindow));
+    }
+
+    void OnGUI()
+    {
+    }
+
+}
---
+++ Scripts/MiscFunctionality/CircleMotion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleMotion : MonoBehaviour {
+
+    public float radius = 1f;
+    public float rotSpeed = 1f;
+    public float heightSpeed = 1f;
+    public float radiusDecrementSpeed = 0.02f;
+
+    private float rot = 1f;
+    private float currentHeight;
+
+
+    void Update()
+    {
+        transform.position = transform.parent.position + new Vector3(Mathf.Sin(rot), currentHeight, Mathf.Cos(rot)) * radius;
+
+        rot += rotSpeed * Time.deltaTime;
+        currentHeight += heightSpeed * Time.deltaTime;
+        radius -= radiusDecrementSpeed * Time.deltaTime;
+    }
+
+}
